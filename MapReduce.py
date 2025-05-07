c7d65443ce16b9c78022138b94f3e39480851a28
Xenusion2110 <nealbbayla@gmail.com>
2025-05-07 11:18:54 -0400

    Update MapReduce.py

--- MapReduce.py
+++ MapReduce.py
@@ -2,7 +2,7 @@ import pandas as pd
 from functools import reduce
 from collections import Counter
 Traffic_data = pd.read_csv("/Users/Neal/Python/BigDataProject/Automated_Traffic_Volume_Counts_20250427.csv",usecols=['Yr', 'M', 'D', 'HH','Boro','street','Vol' ])
-Weather_data = pd.read_csv("/Users/Neal/Python/BigDataProject/final_cleaned_traffic_weather.csv", usecols=['date', 'conditions'])
+Weather_data = pd.read_csv("/Users/Neal/Python/BigDataProject/final_cleaned_traffic_weather.csv", usecols=['Vol', 'is_rain', 'is_snow'])
 
 Traffic_hours = Traffic_data['HH']
 Traffic_hours.tolist()
@@ -29,20 +29,100 @@ def reduce_function_Hotspot(a):
     list = a
     idx = 0
     max_vol = list[2][0]
-    print(max_vol)
     for i in range (1,len(Traffic_data)):
         if list[2][i] > max_vol:
             max_vol = list[2][i]
             idx = i
-    hotspots[0] = list[0][i]
-    hotspots[1] = list[1][i]
-    #print(hotspots[0])
+    hotspots[0] = list[0][idx]
+    hotspots[1] = list[1][idx]
     h = str(hotspots[0]) + " " + str(hotspots[1])
     return h
 
 temp = map_function_Hotspot(Traffic_data)
 print("A Traffic hotspot is " , reduce_function_Hotspot(temp))
 
+#Traffic Volume Trend 
+
+def map_function_Trend(a):
+    List = a
+    s = List['street'].tolist()
+    y = List['Yr']
+    v = List['Vol'].tolist()
+    return [s,y,v]
+
+
+def reduce_function_Trend2(list1, list2):
+    list1 = list1
+    list2 = list2
+    idx = 0
+    max_vol1 = list1[0]
+    max_vol2 = list2[0]
+    for i in list1:
+        if list1[i] > max_vol1:
+            max_vol1 = list1[i]
+            idx = i
+    v1 = idx
+    for i in list2:
+        if list2[i] > max_vol2:
+            max_vol2 = list2[i]
+            idx = i
+    v2 = idx
+    voldiff = v2 - v1
+    trend = (voldiff/v1) * 100
+    return trend
+
+    
+
+def reduce_function_Trend1(list, year):
+    list = list
+    idx = 0
+    v = []
+    year = year
+    for i in range(1, len(Traffic_data)):
+        if list[1][i] == year:
+            v.append(list[2][i])
+    return v
+
+temp = map_function_Trend(Traffic_data)
+l = reduce_function_Trend1(temp, 2015)
+l2 = reduce_function_Trend1(temp, 2024)
+print("The volume for rain has changed by ", reduce_function_Trend2(l,l2))
+
+
+#Most Affected during Hazardous weather
+
+def map_function_Weather(a, b):
+    List = a
+    List2 = b
+    r = List['is_rain']
+    s = List['is_snow']
+    v = List['Vol']
+    st = List2['street']
+    return [r,s,v,st]
+
+def reduce_function_Weather(a):
+    List = a
+    idx = 0
+    idx2 = 0
+    max_vol = List[2][0]
+    for i in range (1, len(Weather_data)):
+        if List[0][i] == True or List[1][i] == True:
+            if List[2][i] > max_vol:
+                max_vol = List[2][i]
+                idx2 = idx
+                idx = i
+    l1 = List[3][idx]
+    l2 = List[3][idx2]
+    return [l1, l2]
+
+temp = map_function_Weather(Weather_data, Traffic_data)
+w = reduce_function_Weather(temp)
+print("The areas most affected by weather are ", w[0], " and ", w[1])
+                
+
+
+
+    
 
 
 
